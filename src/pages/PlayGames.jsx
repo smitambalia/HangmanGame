3f03e282cc444b97f1e3d1363f4e5c0a56075abf
smitambalia <smit.ambalia@gmail.com>
2024-11-12 00:01:28 +0530

    Optimise : code is optimised with destructuring the state varible

--- src/pages/PlayGames.jsx
+++ src/pages/PlayGames.jsx
@@ -1,10 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 export default function PlayGames() {
-    const location = useLocation();
+    const {state} = useLocation();
     return (
         <>
             <h1> Play Game </h1>
-            <h2> {location.state.word} </h2>
+            <h2> {state.word} </h2>
             <Link className="text-blue-400" to="/start">Link to Start Game</Link>
 
         </>
