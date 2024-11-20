5314d7419f76f65a797e12a4e61bee1505e84cb5
smitambalia <smit.ambalia@gmail.com>
2024-11-20 10:27:31 +0530

    Bug : solved only click letter event for the button
    Bug : solved only click letter event for the button

--- src/components/LetterButtons/LetterButtons.jsx
+++ src/components/LetterButtons/LetterButtons.jsx
@@ -1,25 +1,33 @@
 const ALPHABETS = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
 
 export default function LetterButtons({ text, guessedLetters, onLetterClick }) {
-    const originalLetter = text.toUpperCase().split("");
-
-    const buttonStyle = (letter) => {
-        if(guessedLetters.includes(letter)) {
-            return `${originalLetter.includes(letter) ? "bg-green-500" : "bg-red-500"}`;
-        }   
-        else {
-            return `bg-blue-500`;
-        }
+  const originalLetter = text.toUpperCase().split("");
+  console.log("Guessed Letters->", guessedLetters);
+  const buttonStyle = (letter) => {
+    if (guessedLetters.includes(letter)) {
+      return `${
+        originalLetter.includes(letter) ? "bg-green-500" : "bg-red-500"
+      }`;
+    } else {
+      return `bg-blue-500`;
     }
+  };
 
+  function handleLetterClick(event) {
+    const letter = event.target.value;
+    onLetterClick?.(letter);
+  }
   return (
     <>
       {ALPHABETS.map((letter) => {
         return (
           <button
-            className={`h-12 w-12 m-1 text-white rounded-md ${buttonStyle(letter)}`}
+            className={`h-12 w-12 m-1 text-white rounded-md ${buttonStyle(
+              letter
+            )}`}
+            value={letter}
             key={`button-${letter}`}
-            onClick={onLetterClick}
+            onClick={handleLetterClick}
             disabled={guessedLetters.includes(letter)}
           >
             {letter}
--- src/pages/PlayGames.jsx
+++ src/pages/PlayGames.jsx
@@ -1,14 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 import MaskedText from "../components/MaskedText/MaskedText";
 import LetterButtons from "../components/LetterButtons/LetterButtons";
+import { useState } from "react";
 export default function PlayGames() {
   const { state } = useLocation();
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  function handleGuessedLetters(letter) {
+    setGuessedLetters([...guessedLetters,letter]);
+  }
+
+
   return (
     <>
       <h1> Play Game </h1>
       <MaskedText text={state.word} guessedText={[]} />
       <div>
-        <LetterButtons text={state.word} guessedLetters={[]}/>
+        <LetterButtons text={state.word} guessedLetters={guessedLetters} onLetterClick={handleGuessedLetters}/>
       </div>
       <Link className="text-blue-400" to="/start">
         Link to Start Game
