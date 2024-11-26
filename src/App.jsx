9bb8558623d217653d994294e4823e1588c9cfa4
smitambalia <smit.ambalia@gmail.com>
2024-11-26 10:35:57 +0530

    used useContext API for global variable access
    used useContext API for global variable access

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import StartGame from './pages/StartGame';
-import PlayGames from './pages/PlayGames';
-import Home from './pages/Home';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import StartGame from "./pages/StartGame";
+import PlayGames from "./pages/PlayGames";
+import Home from "./pages/Home";
+import { WordContext } from "./context/WordContext";
+import { useState } from "react";
 function App() {
+  const { wordList, setWordList } = useState([]);
 
   return (
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path='/start' element={<StartGame/>}/>
-    <Route path='/play' element={<PlayGames/>} />
-   </Routes>
-  )
+    <WordContext.Provider value={{ wordList, setWordList }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<StartGame />} />
+        <Route path="/play" element={<PlayGames />} />
+      </Routes>
+    </WordContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Button from "../components/Button/Button";
 import { Link } from "react-router-dom";
+import { WordContext } from "../context/WordContext.js";
 
 export default function Home() {
   const [word, setWord] = useState("");
+  const { setWordList } = useContext(WordContext);
+
   async function fetchWords() {
     console.log("get the words");
     const response = await fetch("http://localhost:3000/word");
     const data = await response.json();
+
+    setWordList([...data]);
+
     const randomIndex = Math.floor(Math.random() * data.length);
 
     console.log(data[randomIndex]["wordValue"]);
@@ -15,17 +21,18 @@ export default function Home() {
   }
 
   useEffect(() => {
-    fetchWords();
+     fetchWords();
   }, []);
+  
   return (
     <>
       <Link to="/play" state={{ word: word }}>
-        <Button text="Single Player" styleType="primary"/>
+        <Button text="Single Player" styleType="primary" />
       </Link>
 
       <Link to="/start">
         <div className="mt-4">
-          <Button text="Multi player" styleType="secondary"/>
+          <Button text="Multi player" styleType="secondary" />
         </div>
       </Link>
     </>
